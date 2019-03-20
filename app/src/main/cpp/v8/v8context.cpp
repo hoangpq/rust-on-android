0233feacd3721ca78db76230a45cb5689b499138
Hoang Phan <hoangpq@users.noreply.github.com>
2019-03-20 09:30:02 +0700

    Refactor

--- app/src/main/cpp/v8/v8context.cpp
+++ app/src/main/cpp/v8/v8context.cpp
@@ -96,8 +96,8 @@ void Send(const FunctionCallbackInfo<Value> &args) {
   auto ab = Local<ArrayBuffer>::Cast(v);
   auto contents = ab->GetContents();
 
-  void* raw_cb = createCallback();
-  char* str = workerSendBytes(contents.Data(), ab->ByteLength(), raw_cb);
+  void *raw_cb = createCallback();
+  char *str = workerSendBytes(contents.Data(), ab->ByteLength(), raw_cb);
   args.GetReturnValue().Set(String::NewFromUtf8(isolate_, str));
 }
 
