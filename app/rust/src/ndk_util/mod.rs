77c277ce495a97f78eae70b3ebb4451d48ccfd7e
Hoang Phan <hoangpq@users.noreply.github.com>
2019-06-24 09:42:39 +0700

    update import module

--- app/rust/src/ndk_util/mod.rs
+++ app/rust/src/ndk_util/mod.rs
@@ -1,15 +1,10 @@
 use std::slice;
 
-use jni::{
-    errors::Result,
-    objects::JString,
-    objects::{JString},
-    strings::JavaStr
-};
+use jni::{errors::Result, objects::JString, strings::JavaStr, JNIEnv};
 
 pub fn jni_string_to_string(env: &JNIEnv, s: JString) -> String {
-    let s = JavaStr::from_env(env, s).unwrap();
-    let raw = s.get_raw();
+    let java_str = JavaStr::from_env(env, s).unwrap();
+    let raw = java_str.get_raw();
     unsafe {
         std::str::from_utf8_unchecked(slice::from_raw_parts(raw as *const u8, libc::strlen(raw)))
             .to_string()
